716cb545d2c47982410ecb17fec81a32987d3458
Simon Lomax <simax.99.99@gmail.com>
2016-10-15 14:42:37 +0100

    Added logo and HTML for log filters section

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (attribute, class, href)
+import Html.Attributes exposing (attribute, class, href, src, name, type', placeholder)
 import Html.App as App
 import Http
 import Task
@@ -106,15 +106,210 @@ view : Model -> Html Msg
 view model =
     case model.error of
         Nothing ->
-            logsView
-                model.logs
+            div []
+                [ logViewPageHeader
+                , logFilters
+                , logsView
+                    model.logs
+                ]
 
         _ ->
             div []
-                [ div
+                [ logViewPageHeader
+                , logFilters
+                , div
                     []
                     [ text (Maybe.withDefault "" model.error) ]
                 ]
+
+
+logFilters : Html msg
+logFilters =
+    div [ class "ui segment" ]
+        [ div [ class "ui form" ]
+            [ div [ class "two fields" ]
+                [ div [ class "field" ]
+                    [ div [ class "ui fluid search selection dropdown" ]
+                        [ input [ name "application", type' "hidden", Html.Attributes.value "" ]
+                            []
+                        , i [ class "dropdown icon" ]
+                            []
+                        , input [ class "search", attribute "tabindex" "0", type' "text" ]
+                            []
+                        , div [ class "text" ]
+                            [ text "All" ]
+                        , div [ class "menu", attribute "tabindex" "-1" ]
+                            [ div [ class "item active selected", attribute "data-value" "" ]
+                                [ text "All" ]
+                            , div [ class "item", attribute "data-value" "EKM Powershop" ]
+                                [ text "EKM Powershop" ]
+                            , div [ class "item", attribute "data-value" "Test App" ]
+                                [ text "Test App" ]
+                            ]
+                        ]
+                    ]
+                , div [ class "field" ]
+                    [ div [ class "ui fluid multiple search selection dropdown" ]
+                        [ input [ name "tags", type' "hidden", Html.Attributes.value "" ]
+                            []
+                        , i [ class "dropdown icon" ]
+                            []
+                        , input [ class "search", attribute "tabindex" "0", type' "text" ]
+                            []
+                        , span [ class "sizer" ]
+                            []
+                        , div [ class "default text" ]
+                            [ text "Tags" ]
+                        , div [ class "menu", attribute "tabindex" "-1" ]
+                            [ div [ class "item", attribute "data-value" "aaaa" ]
+                                [ i [ class "blue circle icon" ]
+                                    []
+                                , text "aaaa"
+                                ]
+                            , div [ class "item", attribute "data-value" "bbbb" ]
+                                [ i [ class "blue circle icon" ]
+                                    []
+                                , text "bbbb"
+                                ]
+                            , div [ class "item", attribute "data-value" "cccc" ]
+                                [ i [ class "blue circle icon" ]
+                                    []
+                                , text "cccc"
+                                ]
+                            , div [ class "item", attribute "data-value" "dddd" ]
+                                [ i [ class "blue circle icon" ]
+                                    []
+                                , text "dddd"
+                                ]
+                            , div [ class "item", attribute "data-value" "query" ]
+                                [ i [ class "blue circle icon" ]
+                                    []
+                                , text "query"
+                                ]
+                            , div [ class "item", attribute "data-value" "request" ]
+                                [ i [ class "blue circle icon" ]
+                                    []
+                                , text "request"
+                                ]
+                            , div [ class "item", attribute "data-value" "sqlite" ]
+                                [ i [ class "blue circle icon" ]
+                                    []
+                                , text "sqlite"
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            , div [ class "inline fields" ]
+                [ div [ class "field" ]
+                    [ div [ class "ui checkbox tooltip", attribute "data-content" "Trace" ]
+                        [ input [ name "level[1]", type' "checkbox", Html.Attributes.value "on" ]
+                            []
+                        , label []
+                            [ i [ class "green spy icon" ]
+                                []
+                            ]
+                        ]
+                    ]
+                , div [ class "field" ]
+                    [ div [ class "ui checkbox tooltip", attribute "data-content" "Debug" ]
+                        [ input [ name "level[2]", type' "checkbox", Html.Attributes.value "on" ]
+                            []
+                        , label []
+                            [ i [ class "blue spy icon" ]
+                                []
+                            ]
+                        ]
+                    ]
+                , div [ class "field" ]
+                    [ div [ class "ui checkbox tooltip", attribute "data-content" "Info" ]
+                        [ input [ name "level[3]", type' "checkbox", Html.Attributes.value "on" ]
+                            []
+                        , label []
+                            [ i [ class "blue info icon" ]
+                                []
+                            ]
+                        ]
+                    ]
+                , div [ class "field" ]
+                    [ div [ class "ui checkbox tooltip", attribute "data-content" "Warning" ]
+                        [ input [ name "level[4]", type' "checkbox", Html.Attributes.value "on" ]
+                            []
+                        , label []
+                            [ i [ class "orange warning icon" ]
+                                []
+                            ]
+                        ]
+                    ]
+                , div [ class "field" ]
+                    [ div [ class "ui checkbox tooltip", attribute "data-content" "Error" ]
+                        [ input [ name "level[5]", type' "checkbox", Html.Attributes.value "on" ]
+                            []
+                        , label []
+                            [ i [ class "red warning sign icon" ]
+                                []
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        , h4 [ class "header" ]
+            [ text "Meta" ]
+        , div [ class "ui form" ]
+            [ div [ class "two fields" ]
+                [ div [ class "field" ]
+                    [ input [ name "metaKey", placeholder "Meta Key", type' "text", Html.Attributes.value "" ]
+                        []
+                    ]
+                , div [ class "field" ]
+                    [ input [ name "metaValue", placeholder "Meta Value", type' "text", Html.Attributes.value "" ]
+                        []
+                    ]
+                , button [ class "ui positive circular icon button" ]
+                    [ i [ class "plus icon" ]
+                        []
+                    ]
+                ]
+            ]
+        ]
+
+
+logViewPageHeader : Html msg
+logViewPageHeader =
+    header [ class "ui top menu" ]
+        [ div [ class "item" ]
+            [ a [ href "#/" ]
+                [ img [ attribute "height" "26", src "../static/img/logo.svg" ]
+                    []
+                ]
+            ]
+        , a [ class "item", href "#/monitor" ]
+            [ text "Monitor" ]
+        , a [ class "item", href "#/logs" ]
+            [ text "Logs" ]
+        , a [ class "item", href "#/docs" ]
+            [ text "Docs" ]
+        , div [ class "borderless right menu" ]
+            [ div [ class "ui dropdown item", attribute "style" "min-width: 180px;", attribute "tabindex" "0" ]
+                [ i [ class "facebook icon" ]
+                    []
+                , text " "
+                , text "ekm"
+                , div [ class "menu", attribute "tabindex" "-1" ]
+                    [ a [ class "item", href "#/account" ]
+                        [ i [ class "edit icon" ]
+                            []
+                        , text " My Account"
+                        ]
+                    , a [ class "item", href "#" ]
+                        [ i [ class "sign out icon" ]
+                            []
+                        , text " Logout"
+                        ]
+                    ]
+                ]
+            ]
+        ]
 
 
 tableItem : Log -> Html msg
